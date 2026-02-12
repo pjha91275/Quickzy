1bd8902fdb559317886af14061d12bf6d14c87c9
pjha91275 <pjha91275@gmail.com>
2026-02-12 23:45:33 +0530

    Implementating seeding of DB

---
+++ models/Category.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const CategorySchema = new mongoose.Schema({
+  name:  { type: String, unique: true },
+  count: Number,
+  image: String,
+  bg:    String,
+});
+
+export default mongoose.models.Category || model("Category", CategorySchema);
